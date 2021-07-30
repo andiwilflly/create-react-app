4ad53dd304ac37c0de8274e949732c29c1539855
Vladimir <andiwillfly@gmail.com>
2021-07-30 15:16:15 +0300

    Fix prettier

--- sh/commit.script.sh
+++ sh/commit.script.sh
@@ -5,23 +5,33 @@ END='\033[0m' # ${END}
 # get the argument message
 message="$1"
 
-# Use current date time in the commit message when its not passed
-if [[ -z "${message// }" ]]
-    then
-        echo "${RED} No commit message provided ${END}"
-        exit 1
-fi
+
+{ # try
+
+    # Use current date time in the commit message when its not passed
+  if [[ -z "${message// }" ]]
+      then
+          echo "${RED} No commit message provided ${END}"
+          exit 1
+  fi
 
 
-# stage all changes
-git add .
-echo "${GREEN} Staged all files ${END}"
+  # stage all changes
+  git add .
+  echo "${GREEN} Staged all files ${END}"
 
-# add commit
-git commit -m "$message"
-echo "${GREEN} Added the commit with message: '$message' ${END}"
+  # add commit
+  git commit -m "$message"
+  echo "${GREEN} Added the commit with message: '$message' ${END}"
 
-# get current branch and push
-current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-git push origin "$current_branch"
-echo "${GREEN} Push changes to '$current_branch' branch ${END}
+  # get current branch and push
+  current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+  git push origin "$current_branch"
+  echo "${GREEN} Push changes to '$current_branch' branch ${END}"
+
+
+} || { # catch
+    echo "${RED} Error ${END}"
+}
+
+
