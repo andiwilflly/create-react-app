b1521cd463801d82678a83aa1b8c664fe04cc815
Vladimir <andiwillfly@gmail.com>
2021-07-30 15:14:26 +0300

    Fix prettier

--- sh/commit.script.sh
+++ sh/commit.script.sh
@@ -1,5 +1,6 @@
 RED='\033[0;31m' # ${RED}
 GREEN='\033[0;32m' # ${GREEN}
+END='\033[0m' # ${END}
 
 # get the argument message
 message="$1"
@@ -7,20 +8,20 @@ message="$1"
 # Use current date time in the commit message when its not passed
 if [[ -z "${message// }" ]]
     then
-        echo "${RED} No commit message provided"
+        echo "${RED} No commit message provided ${END}"
         exit 1
 fi
 
 
 # stage all changes
 git add .
-echo "${GREEN} Staged all files"
+echo "${GREEN} Staged all files ${END}"
 
 # add commit
 git commit -m "$message"
-echo "${GREEN} Added the commit with message: '$message'"
+echo "${GREEN} Added the commit with message: '$message' ${END}"
 
 # get current branch and push
 current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 git push origin "$current_branch"
-echo "${GREEN} Push changes to '$current_branch' branch"
+echo "${GREEN} Push changes to '$current_branch' branch ${END}
